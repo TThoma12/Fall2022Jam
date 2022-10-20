5b07bfd67e06d867f8acb43f854a85a1203eb0e7
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2022-10-19 21:35:11 -0400

    Fixed error

--- Slow 2D Platformer/Assets/Scripts/CharacterController.cs
+++ Slow 2D Platformer/Assets/Scripts/CharacterController.cs
@@ -79,7 +79,7 @@ public class CharacterController : MonoBehaviour
     IEnumerator Jump()
     {
         playerRB.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
-        //yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(3.0f);
         //playerRB.velocity = new Vector2(playerRB.velocity.x, -70);
         StopCoroutine(Jump());
     }
