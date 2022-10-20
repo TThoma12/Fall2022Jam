b4982306ddcc5213205dc8c25a3ce625adb1c917
TThoma12 <tthoma12@student.fitchburgstate.edu>
2022-10-20 15:24:11 -0400

    Created new script
    Created player controller script in Visual Studio.

---
+++ Slow 2D Platformer/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // This is responsible for the overall player movement
+    private void FixedUpdate()
+    {
+        // Move
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(2, rb2d.velocity.y);
+        }
+
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-2, rb2d.velocity.y);
+        }
+
+        // Jump
+        if (Input.GetKey("space") || Input.GetKey("w"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 10);
+        }
+    }
+}
