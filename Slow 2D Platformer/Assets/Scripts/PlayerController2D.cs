9ea5b164e8a4cc64a53e15f2116c52963c24e087
TThoma12 <tthoma12@student.fitchburgstate.edu>
2022-10-20 16:06:59 -0400

    Made updates in script
    Added changes to PlayerController2D script.

--- Slow 2D Platformer/Assets/Scripts/PlayerController2D.cs
+++ Slow 2D Platformer/Assets/Scripts/PlayerController2D.cs
@@ -12,6 +12,7 @@ public class PlayerController2D : MonoBehaviour
 
     [SerializeField]
     Transform groundCheck;
+    public float walkSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -44,7 +45,7 @@ public class PlayerController2D : MonoBehaviour
         // Move
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            rb2d.velocity = new Vector2(2, rb2d.velocity.y);
+            rb2d.velocity = new Vector2(walkSpeed, rb2d.velocity.y);
 
             if (isGrounded)
                 animator.Play("Human Clyde Walk");
@@ -54,7 +55,7 @@ public class PlayerController2D : MonoBehaviour
 
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
-            rb2d.velocity = new Vector2(-2, rb2d.velocity.y);
+            rb2d.velocity = new Vector2(-walkSpeed, rb2d.velocity.y);
 
             if (isGrounded)
                 animator.Play("Human Clyde Walk");
